938d1fbf6f20aa2e8549ad064552b0f4fcd0870b
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-25 15:27:20 +0200

    fix: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,49 @@
-import Link from "next/link";
-
-import { LampContainer } from "~/components/ui/lamp";
+import Header from "~/components/header";
+import Socails from "~/components/auth/socials";
+import { Spotlight } from "~/components/ui/spotlight";
 
 import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
-import { CreateTask } from "./_components/create-task";
-import { DeleteTask } from "./_components/delete-task";
 
-export default async function Home() {
+const HomePage = async () => {
   const session = await getServerAuthSession();
 
   return (
-    <LampContainer>
-      <main className="">
-        {/* <Header /> */}
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Task Managment
-          </h1>
+    <div className="bg-grid-white/[0.02] relative flex h-full w-full overflow-hidden bg-black/[0.96] antialiased md:items-center md:justify-center">
+      <Spotlight
+        className="absolute -top-40 left-0 md:-top-20 md:left-60"
+        fill="white"
+      />
+      {session ? (
+        <div className="relative z-10 mx-auto w-full max-w-7xl p-4  md:pt-0">
+          <div className="fixed left-0 top-0 w-full">
+            <Header />
+          </div>
 
-          <div className="flex flex-col items-center gap-2">
-            <div className="flex flex-col items-center justify-center gap-4">
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
+          <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
+            Hello <br /> {session.user.name}
+          </h1>
+        </div>
+      ) : (
+        <div className="relative z-10  mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
+          <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
+            Tasks <br /> are everywhere.
+          </h1>
+          <p className="mx-auto mt-4 max-w-lg text-center text-base font-normal text-neutral-300">
+            Start managing them like a pro and forget how boring they could be
+            and begin new era of healthy habbiits that would completely change
+            your life. This app is designed for those who won&apos;t give up
+            that easy. Are you ready?
+          </p>
+
+          <div className="mt-10 flex w-full justify-center">
+            <div className="w-1/4">
+              <Socails />
             </div>
           </div>
         </div>
-
-        <CrudShowcase />
-      </main>
-    </LampContainer>
-  );
-}
-
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
-
-  const allTask = await api.task.getAll();
-
-  return (
-    <div className="w-full max-w-xs">
-      {allTask.map((task) => (
-        <li key={task.id} className="text-white">
-          <h3>{task.title}</h3>
-          <p>{task.description}</p>
-
-          <DeleteTask id={task.id} />
-        </li>
-      ))}
-
-      <CreateTask />
+      )}
     </div>
   );
-}
+};
+
+export default HomePage;
